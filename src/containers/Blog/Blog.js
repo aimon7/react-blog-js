74dc47286606bc232154fc7fada909fd05b4222c
aimon7 <air.ioannis@gmail.com>
2021-03-13 16:17:04 +0200

    Switching between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
 // import axios from 'axios';
 import Posts from './Posts/Posts';
 import './Blog.css';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render() {
@@ -15,7 +18,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts/>
+                {/*<Route path="/" exact render={() => <h1>Home</h1>}/>*/}
+                {/*<Route path="/" render={() => <h1>Home 2</h1>}/>*/}
+                {/*<Route path="/" exact render={() => <Posts/>}/>*/}
+                <Route path="/" exact component={Posts}/>
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
