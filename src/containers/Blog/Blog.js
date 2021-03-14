92562aa26c67f783f440bbbe011fb73291d46649
aimon7 <air.ioannis@gmail.com>
2021-03-14 19:25:19 +0200

    Redirecting requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { NavLink, Route } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import './Blog.css';
 import NewPost from './NewPost/NewPost';
+import { Redirect } from 'react-router';
 
 class Blog extends Component {
     render() {
@@ -34,7 +35,8 @@ class Blog extends Component {
                 {/*<Route path="/" render={() => <h1>Home 2</h1>}/>*/}
                 {/*<Route path="/" exact render={() => <Posts/>}/>*/}
                 <Route path="/new-post" component={NewPost}/>
-                <Route path="/posts/" component={Posts}/>
+                <Route path="/posts" component={Posts}/>
+                <Redirect from="/" to="/posts" />
             </div>
         );
     }
